cecc0d4a0c75c4693f489763690cc9fed5b839ba
Tony <tunglin.yang1@gmail.com>
2022-10-24 21:59:34 +0800

    加入專案檔案。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtRows = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lblRows = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(157, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "靠左";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtRows
+            // 
+            this.txtRows.Location = new System.Drawing.Point(51, 35);
+            this.txtRows.MaxLength = 2;
+            this.txtRows.Name = "txtRows";
+            this.txtRows.Size = new System.Drawing.Size(100, 22);
+            this.txtRows.TabIndex = 1;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(12, 89);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(346, 231);
+            this.txtResult.TabIndex = 5;
+            this.txtResult.WordWrap = false;
+            // 
+            // lblRows
+            // 
+            this.lblRows.AutoSize = true;
+            this.lblRows.Location = new System.Drawing.Point(12, 38);
+            this.lblRows.Name = "lblRows";
+            this.lblRows.Size = new System.Drawing.Size(32, 12);
+            this.lblRows.TabIndex = 0;
+            this.lblRows.Text = "列數:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(226, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "等腰";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(295, 35);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "靠右";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 352);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblRows);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtRows);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(386, 391);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(386, 391);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtRows;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lblRows;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            //取得列數
+            int? rows = GetRows();
+            //判斷列數必須大於零
+            if (rows.HasValue == false)
+            {
+                MessageBox.Show("請輸入列數");
+            }
+            if (rows.Value < 0)
+            {
+                MessageBox.Show("列數必須大於零");
+            }
+            //生成星號三角形
+            string stars = GenerateStars(rows.Value);
+            //用textbox呈現
+            Display(stars);
+        }
+
+        private void Display(string stars)
+        {
+            txtResult.Text = stars;
+        }
+
+        private string GenerateStars(int rows)
+        {
+            string result = string.Empty;
+            for (int i = 1; i < rows; i++)
+            {
+                result += new string('*', i) + "\r\n";
+            }
+            return result;
+        }
+        private int? GetRows()
+        {
+            string input = txtRows.Text;
+            bool isInt = int.TryParse(input, out int rows);
+            if (isInt)
+            {
+                return rows;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            int? rows = GetRows();
+            
+            if (rows.HasValue == false)
+            {
+                MessageBox.Show("請輸入列數");
+            }
+            if (rows.Value < 0)
+            {
+                MessageBox.Show("列數必須大於零");
+            }
+            //生成星號三角形
+            string stars = GenerateStars2(rows.Value);
+            //用textbox呈現
+            Display(stars);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            int? rows = GetRows();
+            
+            if (rows.HasValue == false)
+            {
+                MessageBox.Show("請輸入列數");
+            }
+            if (rows.Value < 0)
+            {
+                MessageBox.Show("列數必須大於零");
+            }
+            //生成星號三角形
+            string stars = GenerateStars3(rows.Value);
+            //用textbox呈現
+            Display(stars);
+        }
+        private string GenerateStars2(int rows)
+        {
+            string result = string.Empty;
+            for (int i = 1; i < rows; i++)
+            {
+                int countOfEmpty = rows - i;
+                int countOfStar = i * 2 - 1;
+                result += new string(' ', countOfEmpty) + new string('*', countOfStar) + "\r\n";
+            }
+            return result;
+        }
+        private string GenerateStars3(int rows)
+        {
+            string result = string.Empty;
+            for (int i = 1; i < rows; i++)
+            {
+                int countOfEmpty = rows - i;
+                int countOfStar = i;
+                result += new string(' ', countOfEmpty) + new string('*', countOfStar) + "\r\n";
+            }
+            return result;
+        }
+    }
+    
+}
